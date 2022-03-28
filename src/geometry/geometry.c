c6958e3b1040c247bdae3c37704cbff422ce10c3
StepanGO777 <StepanGO777@users.noreply.github.com>
2022-03-28 21:44:26 +0700

    My geometry

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -1,22 +1,27 @@
+#include "libgeo/check.h"
 #include "libgeo/circle.h"
 #include "libgeo/triangle.h"
 #include <math.h>
 #include <stdio.h>
-
+ 
 int main()
 {
-    	  int a;
-        printf("1. Cirle\n2. Triangle\nChoose your figure: ");
-        scanf(" %d", &a);
-        if (a == 1) {
-            circle();
-        }
-        if (a == 2) {
-            triangle();
-        }
-	  else {
-            printf("Error\n");
-            return 0;
-        }
+    double x0, y0, r, x1, y1, x2, y2, x3, y3;
+    printf("Enter (x y rad) for circle\n");
+    scanf("%lf %lf %lf", &x0, &y0, &r);
+    if (r < 0) {
+        printf("Error");
+        return 1;
+    }
+    printf("Enter (x1 y1) for triangle\n");
+    scanf("%lf %lf", &x1, &y1);
+    printf("Enter (x2 y2) for triangle\n");
+    scanf("%lf %lf", &x2, &y2);
+    printf("Enter (x3 y3) for triangle\n");
+    scanf("%lf %lf", &x3, &y3);
+    circle(r);
+    triangle(x1, y1, x2, y2, x3, y3);
+    check(x0, y0, r, x1, y1, x2, y2, x3, y3);
     return 0;
+
 }
