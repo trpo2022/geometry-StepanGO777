6534eaf06ad054ee6f091199a25d17194c7ffa5d
StepanGO777 <StepanGO777@users.noreply.github.com>
2022-03-28 21:45:05 +0700

    My circle

--- src/libgeo/circle.c
+++ src/libgeo/circle.c
@@ -2,15 +2,13 @@
 #include <math.h>
 #include <stdio.h>
 
-void circle()
+void circle(double r)
 {
-    float x, y, r, p, s;
-    printf("Enter (x, y, radius) for circle: ");
-    scanf(" circle(%f, %f, %f)", &x, &y, &r);
+    float p, s;
     if (r > 0) {
         p = M_PI * r * 2;
         s = M_PI * r * r;
-        printf("Perimetr: %.2f\nS: %.2f \n", s, p);
+        printf("Perimetr circle: %.2f\nS circle: %.2f \n", s, p);
     } else {
         printf("Error\n");
     }
