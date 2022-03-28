664a847a8a9996d77b5dd6ccc63a96792f9d63db
StepanGO777 <StepanGO777@users.noreply.github.com>
2022-03-28 21:46:13 +0700

    My triangle.c

--- src/libgeo/triangle.c
+++ src/libgeo/triangle.c
@@ -2,21 +2,28 @@
 #include <math.h>
 #include <stdio.h>
 
-void triangle()
+void triangle(
+	double x1,
+        double y1,
+        double x2,
+        double y2,
+        double x3,
+        double y3)
 {
     float a, b, c, p, p1, s;
-    printf("Enter (a, b, c) sides for triangle: ");
-    scanf(" triangle(%f, %f, %f)", &a, &b, &c);
+
+    a = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    b = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+    c = sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+
     if (a > 0 || b > 0 || c > 0) {
-        if (a <= b + c && b <= c + a && c <= a + b) {
             p = a + b + c;
             p1 = p / 2;
             s = sqrt(p1 * (p1 - a) * (p1 - b) * (p1 - c));
-            printf("Perimetr: %.2f\nS: %.2f\n", p, s);
-        } else {
-            printf("Error\n");
-        }
-    } else {
-        printf("Error\n");
+            printf("Perimetr triangle: %.2f\nS triangle: %.2f\n", p, s);
+    } 
+    else 
+    {
+    printf("Error\n");
     }
 }
